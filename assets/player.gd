64b6615353f52152c204ba09d12cb52c3cbfa2a0
Sander in 't Veld <sanderintveld@hotmail.com>
2022-07-16 11:53:39 +0200

    Eat shards and gems when you move, except when you are full.

--- assets/player.gd
+++ assets/player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+const NUM_SHARDS_NEEDED = 5
+
+var num_shards_consumed = 4
+
 var movement_delay = 0
 var momentum = 0
 
@@ -16,15 +20,21 @@ func _process(delta):
 		var movement = Vector2(0, 0)
 		if Input.is_action_pressed("move_left"):
 			movement.x = -16
+			self.scale.x = -1
 		elif Input.is_action_pressed("move_right"):
 			movement.x = 16
+			self.scale.x = 1
 		elif Input.is_action_pressed("move_up"):
 			movement.y = -16
 		elif Input.is_action_pressed("move_down"):
 			movement.y = 16
 		if movement.length() > 0:
 			var space = get_world_2d().get_direct_space_state()
-			if not space.intersect_point(self.position + movement, 1, [], 1):
+			var mask = 1
+			if num_shards_consumed >= NUM_SHARDS_NEEDED:
+				mask |= 2
+			print(mask)
+			if not space.intersect_point(self.position + movement, 1, [], mask):
 				self.position += movement
 			if momentum > 0.100:
 				movement_delay = 0.070
@@ -33,3 +43,12 @@ func _process(delta):
 			else:
 				movement_delay = 0.250
 			momentum = 0.300
+
+
+func _on_Area2D_body_entered(body):
+	print(body.name)
+	if body is StaticBody2D:
+		body.queue_free()
+		num_shards_consumed += 1
+		if num_shards_consumed >= NUM_SHARDS_NEEDED:
+			$AnimatedSprite.animation = "jumping"
