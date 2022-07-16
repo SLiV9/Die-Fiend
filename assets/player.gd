2193960a97e544d0085ebe343a770d489beed0d3
Sander in 't Veld <sanderintveld@hotmail.com>
2022-07-16 11:02:28 +0200

    Made movement grid based.

--- assets/player.gd
+++ assets/player.gd
@@ -1,19 +1,41 @@
 extends KinematicBody2D
 
-const RUN_SPEED = 400
-
-var velocity = Vector2()
+var held_down_movement = Vector2(0, 0)
 
 func _ready():
 	pass
 
 
-func _physics_process(delta):
-	var control = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if control.length() > 0.01:
-		velocity = control * RUN_SPEED
-	elif velocity.length() > 1:
-		velocity *= 0.5
+func _process(delta):
+	var movement = Vector2(0, 0)
+	if Input.is_action_just_pressed("move_left") or held_down_movement.x < -1:
+		movement.x = -16
+	elif Input.is_action_just_pressed("move_right") or held_down_movement.x > 1:
+		movement.x = 16
+	elif Input.is_action_just_pressed("move_up") or held_down_movement.y < -1:
+		movement.y = -16
+	elif Input.is_action_just_pressed("move_down") or held_down_movement.y > 1:
+		movement.y = 16
 	else:
-		velocity = Vector2(0, 0)
-	move_and_slide(velocity)
+		var charge = 20.0 * delta
+		if Input.is_action_pressed("move_left"):
+			held_down_movement.x -= charge
+			if held_down_movement.x < 0:
+				held_down_movement.y = 0
+		elif Input.is_action_pressed("move_right"):
+			held_down_movement.x += charge
+			if held_down_movement.x > 0:
+				held_down_movement.y = 0
+		elif Input.is_action_pressed("move_up"):
+			held_down_movement.y -= charge
+			if held_down_movement.y < 0:
+				held_down_movement.x = 0
+		elif Input.is_action_pressed("move_down"):
+			held_down_movement.y += charge
+			if held_down_movement.y > 0:
+				held_down_movement.x = 0
+	if movement.length() > 0:
+		held_down_movement = Vector2(0, 0)
+		var space = get_world_2d().get_direct_space_state()
+		if not space.intersect_point(self.position + movement, 1, [], 1):
+			self.position += movement
