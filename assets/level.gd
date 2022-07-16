63a4c024ce6cc262471bd0988228a69c33906cac
Sander in 't Veld <sanderintveld@hotmail.com>
2022-07-16 21:02:02 +0200

    Replaced Area2D overlap checks with just calculating the bounding box.

--- assets/level.gd
+++ assets/level.gd
@@ -31,26 +31,33 @@ func spawn_shard_in_room(i):
 
 
 func remove_item_in_room(i):
-	var rooms = [$Room1, $Room2, $Room3, $Room4, $Room5, $Room6]
-	var room = rooms[i]
-	var bodies = room.get_overlapping_bodies()
+	var bbox = room_bbox(i)
 	var targets = []
-	for body in bodies:
-		if body is Gem:
-			targets.append(body)
-		elif body is Shard:
-			targets.append(body)
+	for child in get_children():
+		if child is Gem or child is Shard:
+			if bbox.has_point(child.position):
+				targets.append(child)
 	if targets.size() > 0:
 		targets.shuffle()
 		var item = targets.pop_front()
 		item.queue_free()
 
 
+func room_bbox(i):
+	var rooms = [$Room1, $Room2, $Room3, $Room4, $Room5, $Room6]
+	var room = rooms[i]
+	var center = room.position
+	var bbox = Rect2(center.x - 16 * ROOM_SIZE / 2,
+		center.y - 16 * ROOM_SIZE / 2,
+		16 * ROOM_SIZE,
+		16 * ROOM_SIZE)
+	return bbox
+
+
 func _on_Gremlin_shard_consumed():
 	var best_i = randi() % 6
-	var rooms = [$Room1, $Room2, $Room3, $Room4, $Room5, $Room6]
 	for i in range(0, 6):
-		if rooms[i].overlaps_body($Gremlin):
+		if room_bbox(i).has_point($Gremlin.position):
 			best_i = 5 - i
 	self.call_deferred("spawn_shard_in_room", best_i)
 
@@ -58,27 +65,22 @@ func _on_Gremlin_shard_consumed():
 func _on_Gremlin_gem_deposited():
 	var gem = $ResourcePreloader.get_resource("Gem").instance()
 	gem.position = $Gremlin.position
-	var rooms = [$Room1, $Room2, $Room3, $Room4, $Room5, $Room6]
 	for i in range(0, 6):
-		if rooms[i].overlaps_body($Gremlin):
+		if room_bbox(i).has_point($Gremlin.position):
 			gem.get_node("AnimatedSprite").modulate = room_colors[i]
 	add_child(gem)
 
 
 func _on_Roller_rolled():
 	var weights = [0, 0, 0, 0, 0, 0]
-	var rooms = [$Room1, $Room2, $Room3, $Room4, $Room5, $Room6]
-	for i in range(0, 6):
-		var bodies = rooms[i].get_overlapping_bodies()
-		for body in bodies:
-			if body is Gem:
-				weights[i] += 4
-			elif body is Shard:
-				weights[i] += 1
-	print(weights)
 	for child in get_children():
-		if child is Gem:
-			print(child, child.collision_layer, child.collision_mask)
+		if child is Shard or child is Gem:
+			for i in range(0, 6):
+				if room_bbox(i).has_point(child.position):
+					if child is Gem:
+						weights[i] += 4
+					elif child is Shard:
+						weights[i] += 1
 	$Hero/Roller.roll(weights)
 
 func _on_MonsterRoller_rolled():
@@ -90,11 +92,3 @@ func _on_Roller_roll_determined(results):
 		remove_item_in_room(result - 1)
 		var opposite_face = 7 - result
 		spawn_shard_in_room(opposite_face - 1)
-
-
-func _on_Room1_body_entered(body):
-	print(body, " entered")
-
-
-func _on_Room1_body_exited(body):
-	print(body, " exited")
